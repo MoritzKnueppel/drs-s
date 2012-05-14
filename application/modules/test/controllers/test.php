85fc221de69125c7df918764129babedfd7e34c5
Moritz Knueppel <moritz-knueppel@live.de>
2012-05-14 17:13:28 +0200

    navbar and bootstrap

--- application/modules/test/controllers/test.php
+++ application/modules/test/controllers/test.php
@@ -15,7 +15,8 @@ function blog()
     $this->grocery_crud->unset_columns('deleted');
     $this->grocery_crud->display_as('content', 'Inhalt');
     $output = $this->grocery_crud->render('blog');
-    $data['view']='test_view';  
+    $data['view']='test_view'; 
+    $data['grocery_stylesheet']=true; 
     $this->load->vars($data);
          
     $this->_example_output($output);
--- application/views/header.php
+++ application/views/header.php
@@ -1,47 +1,20 @@
 <html>
 <head>
-   
-<?php 
-foreach($css_files as $file): ?>
-    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
 
-<?php endforeach; ?>
+<link href="<?=base_url()?>assets/bootstrap/css/bootstrap.css" rel="stylesheet"> 
 
-<?php foreach($js_files as $file): ?>
- 
-    <script src="<?php echo $file; ?>"></script>
-<?php endforeach; ?>
- 
-<style type='text/css'>
-body
-{
-    font-family: Arial;
-    font-size: 14px;
-}
-a {
-    color: blue;
-    text-decoration: none;
-    font-size: 14px;
-}
-a:hover
-{
-    text-decoration: underline;
-}
-</style>
+<?php
+    if(isset($grocery_stylesheet)){
+        $this->load->file('assets/grocery_crud/stylesheet.php');
+    }
+?>   
 
-<link href="assets/bootstrap/css/bootstrap.css" rel="stylesheet">
+
 
 </head>
 <body> 
 
   
-
-
-<ul class="nav nav-tabs">
-  <li class="active">
-    <a href="#">Home</a>
-  </li>
-  <li><a href="#">...</a></li>
-  <li><a href="#">...</a></li>
-</ul>
- 
+<?php
+    $this->load->view('navigation');   
+?>
