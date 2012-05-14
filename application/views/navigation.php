3044bc3826e1f3d2842d829b5aacd6fe4b692232
Konstantin <konstantin.schmidt.home@googlemail.com>
2012-05-14 17:20:40 +0200

    tabs

--- application/views/navigation.php
+++ application/views/navigation.php
@@ -1,8 +1,11 @@
 
 <ul class="nav nav-tabs">
   <li class="active">
-    <a href="#">Home</a>
+    <a href="fibuhome">Home</a>
   </li>
-  <li><a href="#">...</a></li>
-  <li><a href="#">...</a></li>
+  <li><a href="basicdata">Stammdaten</a></li>
+  <li><a href="inbox">Posteingang</a></li>
+  <li><a href="car">Fahrzeug</a></li>
+  <li><a href="invoice">Rechnung</a></li>
+  
 </ul>
