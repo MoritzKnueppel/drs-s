a91c0c1f860cb3b97587d1f7d096bf4001bdac56
Moritz Knueppel <moritz-knueppel@live.de>
2012-05-14 14:13:03 +0200

    DB

--- application/modules/test/controllers/test.php
+++ application/modules/test/controllers/test.php
@@ -6,9 +6,11 @@ class Test extends CI_Controller
 
 function blog()
 {
+   
     $this->load->library('grocery_crud');
     $this->grocery_crud->unset_delete();
-    $this->grocery_crud->unset_edit();
+    $this->grocery_crud->unset_edit(); 
+    $this->grocery_crud->set_theme('datatables');
     $this->grocery_crud->unset_columns('deleted');
     $this->grocery_crud->display_as('content', 'Inhalt');
     $output = $this->grocery_crud->render('blog');
