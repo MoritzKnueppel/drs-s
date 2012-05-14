79d2841a9ce5b092be1b788093fede3c12524cde
DRS-hagelschaden <jan.kube01@googlemail.com>
2012-05-14 22:56:23 +0200

    Kommentierung fr Programmierung

--- application/modules/car/controllers/car.php
+++ application/modules/car/controllers/car.php
@@ -2,10 +2,22 @@
 
 class Car extends CI_Controller {
 
-	public function index()
-	{
-    $data['text'] = "Ich bin das CAR Modul";
-		$data['view'] = 'car_view';
+public function index()
+{
+//  Erstmal nur eine Übersicht erzeugen, von der es auf die anderen Funktionen weiterführt
+//  Wahrscheinlich kommen hier mal Auzählungen oder Gliederungen rein sowie ein Suchfeld ein
+//  $data['quantity']   =   $this->car_model->car_count_all();
+//  $data['title']      =   'Fahrzeuge/Vorg&auml;nge' 
+    $data['text']       =   "Ich bin das CAR Modul";
+    $data['view']       =   'car_view';
     $this->load->view('container', $data);
-	}
+//  View mit Nennung der Einträge. Klick auf Eintragsmenge führt zu car/car_list('quantity')
 }
+
+public function cars_list($group)
+{
+    //  $data['cars']   =   $this->car_model->cars_get_by($group);   
+    //  Fahrzeugdaten über container im car_list_view ausgeben   
+}
+
+} // END CONTROLLER car.php
