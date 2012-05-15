ba214a467bcc4b3baf5f061fa0a4782035a69496
Artur <sinus-@web.de>
2012-05-15 10:36:02 +0200

    pdfs erstellt

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = 'blog';
+$db['default']['username'] = 'drs';
+$db['default']['password'] = 'lamue2011';
+$db['default']['database'] = 'spring';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
